f8913f1660fb981d403d5957e114462ec8c6d5c3
antou <andong041205@gmail.com>
2026-01-23 23:32:20 +0900

    update: 修飾子について理解した

--- dart_playground/main.dart
+++ dart_playground/main.dart
@@ -10,4 +10,21 @@ void main() {
   // こっちでも可
   // print('Name is ${name}');
 
+
+  // 配列
+  const List<String> fruits = ['りんご','バナナ','みかん'];
+  print(fruits);
+
+  final Set<int> nums = {1,81,44,82,86,886};
+  print(nums);
+
+  final Map<String, dynamic> info = {'name':'Blake', 'age':16};
+  final String greeting = "${info['name']}, ${info['age']}";
+  print(greeting);
+
+  // 動的型付け (あまり使わないように)
+  dynamic things;
+  things = '2';
+  things = 1;
+  print(things);
 }
---
+++ dart_playground/main1.dart
@@ -0,0 +1,11 @@
+void main() {
+  // 基本は varで行こう
+  var data = 0;
+  print(data);
+
+  data = 15;
+  print(data);
+
+  final now = DateTime.now();
+  print(now);
+}
