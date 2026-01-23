19a7cece0a0d1d36f215c8faf3e83af96286a816
antou <andong041205@gmail.com>
2026-01-23 22:19:33 +0900

    add: 変数宣言確認

--- dart_playground/main.dart
+++ dart_playground/main.dart
@@ -1,4 +1,13 @@
 void main() {
-  print('Hello Dart!');
-  print("こんにちは");
+  // print('Hello Dart!');
+  // print("こんにちは");
+
+  const int number = 150;
+  final String name = 'John';
+
+  print(number);
+  print('Hello $name');
+  // こっちでも可
+  // print('Name is ${name}');
+
 }
