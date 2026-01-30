d9899f6e7068bd7e8b54cd0908681573dfe5e9e3
antou <andong041205@gmail.com>
2026-01-30 19:26:42 +0900

    first commit

---
+++ qiita_search/lib/screens/search_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// class SearchScreen extends StatefulWidget {
+//   const SearchScreen({super.key});
+
+//   // まず、StatefulWidgetを使ったのはなぜ？lessとの使い分けは？
+//   // finalはいつ入れるの？
+//   // このsuper.keyって何？
+
+//   @override
+// }
