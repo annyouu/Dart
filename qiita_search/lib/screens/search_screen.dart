83654d7ad1cd879e180750e2b59ac1d3a6dda4ab
antou <andong041205@gmail.com>
2026-01-30 20:49:06 +0900

    [add] ページ追加

--- qiita_search/lib/screens/search_screen.dart
+++ qiita_search/lib/screens/search_screen.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 
-// class SearchScreen extends StatefulWidget {
-//   const SearchScreen({super.key});
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({super.key});
 
-//   // まず、StatefulWidgetを使ったのはなぜ？lessとの使い分けは？
-//   // finalはいつ入れるの？
-//   // このsuper.keyって何？
+  // まず、StatefulWidgetを使ったのはなぜ？lessとの使い分けは？
+  // finalはいつ入れるの？
+  // このsuper.keyって何？
 
-//   @override
-// }
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Qiita Search'),
+      ),
+      body: Container(),
+    );
+  }
+}
